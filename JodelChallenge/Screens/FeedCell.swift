54d42d213b530a4a9afcec889ab5875403b30770
Johannes Unruh <johannes@unruh.ai>
2019-07-07 10:35:31 +0200

    added: label; changed: layout of uicollctionviewcells

--- JodelChallenge/Screens/FeedCell.swift
+++ JodelChallenge/Screens/FeedCell.swift
@@ -11,12 +11,15 @@ import UIKit
 class FeedCell : UICollectionViewCell {
     
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
     
     public func configure(with imageInformation : Dictionary<AnyHashable, Any>) {
         let imageUrl = imageInformation["url"] as! URL
+        let title = imageInformation["title"] as! String
         if let data = try? Data(contentsOf: imageUrl) {
             let image = UIImage(data: data)
             imageView.image = image
+            titleLabel.text = title
         }
     }
 }
--- JodelChallenge/Screens/FeedViewController.swift
+++ JodelChallenge/Screens/FeedViewController.swift
@@ -11,6 +11,9 @@ import UIKit
 class FeedViewController : UICollectionViewController {
     
     var data : [Dictionary<AnyHashable, Any>] = []
+    private let reuseIdentifier = "FeedCell"
+    private let sectionInsets = UIEdgeInsets(top: 50.0, left: 20.0, bottom: 50.0, right: 20.0)
+    private let itemsPerRow: CGFloat = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,14 +25,40 @@ class FeedViewController : UICollectionViewController {
             })
         }
     }
+}
+
+// UICollectionViewDataSource methods
+extension FeedViewController {
+    override func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return data.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "FeedCell", for: indexPath) as! FeedCell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! FeedCell
         cell.configure(with: data[indexPath.row])
         return cell
+    }
+}
+
+extension FeedViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let paddingSpace = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = view.frame.width - paddingSpace
+        let widthPerItem = availableWidth / itemsPerRow
+        
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
     }
 }
