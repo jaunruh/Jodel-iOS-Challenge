0cf2654e5484f34bd59e70ab50bf6f294eea0ee4
Johannes Unruh <johannes@unruh.ai>
2019-07-10 10:43:49 +0200

    added: unittest for flickrapi; no ui unittests yet

--- JodelChallengeTests/JodelChallengeTests.swift
+++ JodelChallengeTests/JodelChallengeTests.swift
@@ -11,9 +11,13 @@ import XCTest
 
 class JodelChallengeTests: XCTestCase {
     
+    var app: XCUIApplication!
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        app = XCUIApplication()
+        app.launch()
     }
     
     override func tearDown() {
@@ -33,4 +37,54 @@ class JodelChallengeTests: XCTestCase {
         }
     }
     
+    @objc func testFlickrApi() {
+        // Create expectation in order to unit test asynchronously
+        let myExpectation = expectation(description: "Fetch data from Flickr")
+        
+        // Call fetch function from NetworkAPI
+        FlickrApi.fetchPhotos(withPageNumber: 1, andCompletion: { (responseArray, error) in
+//            usleep(3000000) // -> see if test fails if timeout is reached
+            
+            // Avoid force unwrapping
+            guard let responseArray = responseArray else {
+                XCTFail("No response")
+                return
+            }
+            
+            // Asssert a few properties on the returned data
+            XCTAssertTrue(responseArray.count == 10)
+            let myFirstObject = responseArray.first!
+            XCTAssertTrue(myFirstObject["url"] != nil)
+            XCTAssertTrue(myFirstObject["title"] != nil)
+            XCTAssertTrue(myFirstObject["id"] != nil)
+            
+            // Fullfill expectation if all assertions are met
+            myExpectation.fulfill()
+        })
+        
+        // Fail test after 2 seconds wait time
+        waitForExpectations(timeout: 2, handler: { error in
+            if let error = error {
+                XCTFail("Timeout: \(error)")
+            }
+        })
+    }
+    
+// UI Testing probably not working because the imageview is not actually layed out.
+// How would one unit test ui components?
+    
+//    func testFeedCell() {
+//
+//        // I chose to get an image url from a placeholder page instead of asynchronously calling the
+//        // api and using an image from the response.
+//        // Test will obviously fail if placeholder.com api goes offline
+////        let myData: Dictionary<AnyHashable, Any> = ["url": "https://via.placeholder.com/250.png", "title": "mytitle"]
+//
+//        // For some reason creating the cell using this dictionary the force conversion to url in the FeedCell fails.
+//        // Even though objects in the debugger are identical.
+////        let myData: Dictionary<AnyHashable, Any> = ["url": "https://farm66.static.flickr.com/65535/48239102577_329c07f3a7_m.jpg", "title": "mytitle"]
+//
+//        let myFeedCell = FeedCell.init()
+//        myFeedCell.configure(with: myData)
+//    }
 }
--- Podfile
+++ Podfile
@@ -3,3 +3,8 @@ platform :ios, '10.0'
 target 'JodelChallenge' do
   pod 'FlickrKit'
 end
+
+target 'JodelChallengeTests' do
+  inherit! :search_paths
+  # Pods for testing
+end
